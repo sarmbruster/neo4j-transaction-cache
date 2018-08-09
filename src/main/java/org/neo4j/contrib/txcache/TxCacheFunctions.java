2cf1ec99aad4a000f0bc3c4fb677563ea3bfe0be
Stefan Armbruster <stefan@armbruster-it.de>
2018-08-09 11:11:53 +0200

    autoflush cache upon new transaction

--- src/main/java/org/neo4j/contrib/txcache/TxCacheFunctions.java
+++ src/main/java/org/neo4j/contrib/txcache/TxCacheFunctions.java
@@ -1,5 +1,8 @@
 package org.neo4j.contrib.txcache;
 
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.logging.Log;
+import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 import org.neo4j.procedure.UserFunction;
@@ -11,17 +14,40 @@ import java.util.stream.Stream;
 public class TxCacheFunctions {
 
     static ThreadLocal<Map<String, Object>> cacheThreadLocal = ThreadLocal.withInitial(HashMap::new);
+    static ThreadLocal<Long> lastTxStartTimeThreadLocal = ThreadLocal.withInitial(() -> -1l);
+
+    @Context
+    public KernelTransaction kernelTransaction;
+
+    @Context
+    public Log log;
+
+    public static Map<String,Object> getCache(KernelTransaction kernelTransaction) {
+
+        long lastTxStartTime = lastTxStartTimeThreadLocal.get();
+        long thisTxStartTime = kernelTransaction.startTime();
+        if (lastTxStartTime!=thisTxStartTime) {
+            //log.info("we have a different tx on this thread, using a new cache");
+            Map<String,Object> cache = new HashMap<>();
+            cacheThreadLocal.set(cache);
+            lastTxStartTimeThreadLocal.set(thisTxStartTime);
+            return cache;
+        } else {
+            //log.info("we have a known tx on this thread, reusing cache");
+            return cacheThreadLocal.get();
+        }
+    }
 
     @Procedure
     public Stream<MapResult> put(@Name("key") String key, @Name("value") Object value) {
-        Map<String, Object> cache = cacheThreadLocal.get();
+        Map<String, Object> cache = getCache(kernelTransaction);
         cache.put(key, value);
         return Stream.of(new MapResult(cache));
     }
 
     @UserFunction
     public Object get(@Name("key") String key, @Name(value = "clearCache", defaultValue = "false") boolean clearCache) {
-        Map<String, Object> cache = cacheThreadLocal.get();
+        Map<String, Object> cache = getCache(kernelTransaction);
         if (clearCache) {
             cacheThreadLocal.remove();
         }
@@ -30,7 +56,7 @@ public class TxCacheFunctions {
 
     @Procedure
     public Stream<MapResult> add(@Name("key") String key, @Name("value") long increment) {
-        Map<String, Object> map = cacheThreadLocal.get();
+        Map<String, Object> map = getCache(kernelTransaction);
         long value = (long) map.get(key) + increment;
         map.put(key, value);
         return Stream.of(new MapResult(map));
@@ -38,7 +64,7 @@ public class TxCacheFunctions {
 
     @Procedure
     public Stream<MapResult> clear() {
-        Map<String, Object> map = cacheThreadLocal.get();
+        Map<String, Object> map = getCache(kernelTransaction);
         cacheThreadLocal.remove();
         return Stream.of(new MapResult(map));
     }
@@ -50,6 +76,5 @@ public class TxCacheFunctions {
             this.map = map;
         }
     }
-
 
 }
--- src/test/java/org/neo4j/contrib/txcache/TxCacheFunctionsTest.java
+++ src/test/java/org/neo4j/contrib/txcache/TxCacheFunctionsTest.java
@@ -2,52 +2,87 @@ package org.neo4j.contrib.txcache;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.harness.junit.Neo4jRule;
+import org.neo4j.helpers.collection.Iterables;
 import org.neo4j.helpers.collection.Iterators;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class TxCacheFunctionsTest {
 
     @Rule
     public Neo4jRule neo4j = new Neo4jRule()
+            .withConfig(GraphDatabaseSettings.procedure_unrestricted, "org.neo4j.contrib.txcache.*")
             .withProcedure(TxCacheFunctions.class)
             .withFunction(TxCacheFunctions.class);
 
     @Test
     public void shouldCacheIncrementWork() {
-        long counter = Iterators.single(neo4j.getGraphDatabaseService().execute(
+        long counter = singleResultCypher(
                 "CALL org.neo4j.contrib.txcache.put('counter', 1) YIELD map AS dummy1 " +
                         "CALL org.neo4j.contrib.txcache.add('counter', 1) YIELD map AS dummy2 " +
-                        "RETURN org.neo4j.contrib.txcache.get('counter', true) AS counter"
-        ).columnAs("counter"));
+                        "RETURN org.neo4j.contrib.txcache.get('counter', true)");
         assertEquals(2, counter);
         assertTrue(TxCacheFunctions.cacheThreadLocal.get().isEmpty());
     }
 
     @Test
     public void shouldCacheWorkWithMultipleInvocations() {
-        long counter = Iterators.single(neo4j.getGraphDatabaseService().execute(
+        long counter = singleResultCypher(
                 "CALL org.neo4j.contrib.txcache.put('counter', 1) YIELD map AS dummy1 " +
                         "UNWIND range(0,9) AS x CALL org.neo4j.contrib.txcache.add('counter', 1) YIELD map AS dummy2 " +
                         "WITH COUNT(*) AS dummy3 " +
-                        "RETURN org.neo4j.contrib.txcache.get('counter', true) AS counter"
-        ).columnAs("counter"));
+                        "RETURN org.neo4j.contrib.txcache.get('counter', true)");
         assertEquals(11, counter);
         assertTrue(TxCacheFunctions.cacheThreadLocal.get().isEmpty());
     }
 
     @Test
     public void shouldClearWork() {
-        long counter = Iterators.single(neo4j.getGraphDatabaseService().execute(
+        long counter = singleResultCypher(
                 "CALL org.neo4j.contrib.txcache.put('counter', 1) YIELD map AS dummy1 " +
                         "CALL org.neo4j.contrib.txcache.add('counter', 1) YIELD map AS dummy2 " +
                         "CALL org.neo4j.contrib.txcache.clear() YIELD map AS cache " +
-                        "RETURN cache.counter AS counter"
-        ).columnAs("counter"));
+                        "RETURN cache.counter");
         assertEquals(2, counter);
         assertTrue(TxCacheFunctions.cacheThreadLocal.get().isEmpty());
+    }
+
+    @Test
+    public void shouldDifferentTransactionsHaveDifferentCacheWithoutExplicitCleaning() {
+
+        // NB: the two cypher statements in this thes are executed in separate transactions, so the cache will be flushed
+        Map<String,Object> cache = singleResultCypher("CALL org.neo4j.contrib.txcache.put('counter', 1) YIELD map RETURN map");
+        assertEquals(1, cache.size());
+        assertEquals(1l, cache.get("counter"));
+
+        Object value = singleResultCypher("RETURN org.neo4j.contrib.txcache.get('counter')");
+        assertNull(value);
+    }
+
+    @Test
+    public void shouldTransactionsMaintainCacheForMultipleStatements() {
+
+        try (Transaction tx = neo4j.getGraphDatabaseService().beginTx()) {
+
+            // NB: the two cypher statements in this thes are executed in separate transactions, so the cache will be flushed
+            Map<String,Object> cache = singleResultCypher("CALL org.neo4j.contrib.txcache.put('counter', 1) YIELD map RETURN map");
+            assertEquals(1, cache.size());
+            assertEquals(1l, cache.get("counter"));
+
+            long value  = singleResultCypher("RETURN org.neo4j.contrib.txcache.get('counter')");
+            assertEquals(1l, value);
+            tx.success();
+        }
+    }
+
+    private <T> T singleResultCypher(String cypher) {
+        Map<String, Object> row = Iterators.single(neo4j.getGraphDatabaseService().execute(cypher));
+        return (T) Iterables.singleOrNull(row.values());
     }
 
 }
